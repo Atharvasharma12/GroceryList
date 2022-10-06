48f456ee8e9bbf328aba0837210b2e43b94e120f
Atharva sharma <89251910+Atharvasharma12@users.noreply.github.com>
2022-10-06 13:59:00 +0530

    this is v1

---
+++ main.js
@@ -0,0 +1,17 @@
+function additemtlist(){
+    let newitem = document.getElementById("InsertItem").value
+    if (newitem==''){
+        document.getElementById("showItems").innerHTML=("Are bhai kuch to likh ")
+        
+    }
+    else{
+        // document.getElementById("showItems").innerHTML=(newitem)
+        let list = document.getElementById("ul")
+        
+
+    }
+
+
+}
+
+
