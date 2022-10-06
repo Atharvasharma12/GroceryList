6f32e957e778444208b983a2e8de73042df0092f
Atharva sharma <89251910+Atharvasharma12@users.noreply.github.com>
2022-10-06 15:11:08 +0530

    item is removed with error

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ function additemtlist(){
     }
     else{
         // document.getElementById("showItems").innerHTML=(newitem)
-        let list = document.getElementById("ul")
-        
-
+        let list = document.getElementById("box")
+        let li = document.createElement('li')
+        li.textContent=newitem
+        list.appendChild (li)
+        let a = document.createElement('a')
+        a.href = "javascript:void(0)"
+        a.textContent="x"
+        li.appendChild(a)
+        document.getElementById("InsertItem").value =""
     }
+    
 
-
-}
-
-
+    let btn = document.querySelector('ul')
+        btn.addEventListener('click',function(e){
+        let list = document.getElementById('box')
+        let li = e.target.parentNode
+            box.removeChild(li)
+        }
+        )
+    }
